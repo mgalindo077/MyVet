7ec07462cf5b2de2d250ce407235eda0600f361b
Miguel Galindo <mgalindo077@hotmail.com>
2019-12-31 18:13:30 -0600

    Video 81

--- MyVet.Prism/MyVet.Prism/App.xaml.cs
+++ MyVet.Prism/MyVet.Prism/App.xaml.cs
@@ -58,6 +58,7 @@ namespace MyVet.Prism
             containerRegistry.RegisterForNavigation<ProfilePage, ProfilePageViewModel>();
             containerRegistry.RegisterForNavigation<RegisterPage, RegisterPageViewModel>();
             containerRegistry.RegisterForNavigation<RememberPasswordPage, RememberPasswordPageViewModel>();
+            containerRegistry.RegisterForNavigation<ChangePasswordPage, ChangePasswordPageViewModel>();
         }
     }
 }
